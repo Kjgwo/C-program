c37d3aeb3cae5d842959d1d7bc6d6ef0612c52be
김정우 <93897045+Kjgwo@users.noreply.github.com>
2022-05-23 22:56:35 +0900

    Console Tetris 게임 만들기

---
+++ Tetris/Tetris/Header.h
@@ -0,0 +1,93 @@
+#include "TetrisHelper.h"
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+#define GRID_WIDTH 7
+#define GRID_HEIGHT 10
+#define USERBBLOCK_SIZE 3
+
+
+int displayData[GRID_HEIGHT][GRID_WIDTH] = { 0, };
+
+class Display {
+public:
+	void draw() {
+		for (int i = 0; i < GRID_HEIGHT; i++) {
+			for (int k = 0; k < GRID_WIDTH; k++) {
+				drawPosition(k, i, displayData[i][k] == 1);
+			}
+		}
+	}
+};
+
+class GameEngine {
+public:
+	int gameGridData[GRID_HEIGHT][GRID_WIDTH] = {
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 1, 0, 0, 0, 0, 0},
+		{1, 1, 0, 0, 0, 0, 0},
+		{1, 1, 0, 0, 0, 0, 0}
+	};
+	int userBlock[USERBBLOCK_SIZE][USERBBLOCK_SIZE] = {
+		{1, 1, 1},
+		{1, 0, 0},
+		{1, 0, 0}
+	};
+	int blockX = 0;
+	int blockY = 0;
+
+	float elapsed = 0.0f;
+
+	void init() {
+		// 게임 엔진 초기화
+	}
+
+	// while 루프에서 매번 불려지는 함수
+	void next(float dt, char keyboardInput) {
+		elapsed += dt;
+		if (elapsed >= 0.5f) {
+			blockY++;
+			elapsed -= 0.5f;
+		}
+
+	}
+
+	// 실제 게임 데이터를 화면에 출력할 수 있는 데이터로 변환
+	void makeDisplayData() {
+		for (int i = 0; i < GRID_HEIGHT; i++) {
+			for (int k = 0; k < GRID_WIDTH; k++) {
+				displayData[i][k] = gameGridData[i][k];
+			}
+		}
+
+		for (int i = 0; i < USERBBLOCK_SIZE; i++) {
+			for (int k = 0; k < USERBBLOCK_SIZE; k++) {
+				if (i + blockY < 0 || i+ blockY > GRID_HEIGHT) {
+					// DO NOTHING
+				}
+				else if (k + blockX < 0 || k + blockX > GRID_WIDTH) {
+					// DO NOTHING
+				}
+				else {
+					// displayData[i + blockY][k + blockX] = userBlock[i][k] == 1 ? userBlock[i][k] : displayData[i + blockY][k + blockX];
+					int _x = k + blockX;
+					int _y = i + blockY;
+					displayData[_y][_x] = userBlock[i][k] | displayData[_y][_x];
+				}
+
+			}
+		}
+	}
+};
---
+++ Tetris/Tetris/Source.cpp
@@ -0,0 +1,38 @@
+#include "Header.h"
+
+#define INTERVAL 1.0/60.0
+int main() {
+
+
+	float prev = (float)clock() / CLOCKS_PER_SEC;
+	Display* display = new Display();
+	GameEngine* gameEngine = new GameEngine();
+	showConsoleCursor(false);
+
+	while (true) {
+		float curr = (float)clock() / CLOCKS_PER_SEC;
+		float dt = curr - prev;
+
+		if (dt < INTERVAL) continue;
+		prev = curr;
+
+
+		bool left = keyState('a');
+		if (left) {
+			// 왼쪽으로 이동
+		}
+		else {
+			// 그냥 떨어지게
+		}
+
+		gameEngine->next(dt, 0);
+
+		gameEngine->makeDisplayData();
+
+		// 화면 출력
+		display->draw();
+		// 게임 상태 판별
+		bool right = keyState('d');
+	}
+	return 0;
+}
